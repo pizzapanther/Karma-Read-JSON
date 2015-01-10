a1b47d03e944ff9441f821dc270ed76780dc5dae
julianromera <julian.romera@gmail.com>
2015-01-10 07:53:47 +0000

    changed path variable. minor cosmetic modifications

--- karma-read-json.js
+++ karma-read-json.js
@@ -1,27 +1,28 @@
-function readJSON (url) {
-  url = '/base/' + url;
-  
-  var xhr = new XMLHttpRequest();
-  var json = null;
-  
-  xhr.open("GET", url, false);
-  
-  xhr.onload = function (e) {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        json = JSON.parse(xhr.responseText);
-      }
-      
-      else {
-        console.error('readJSON', xhr.statusText);
-      }
-    }
-  };
-  
-  xhr.onerror = function (e) {
-    console.error('readJSON', xhr.statusText);
-  };
-  
-  xhr.send(null);
-  return json;
-}
+(function(window, document, undefined) {
+    'use strict';
+
+    window.readJSON = function(url) {
+        var xhr = new XMLHttpRequest();
+        var json = null;
+
+        xhr.open("GET", url, false);
+
+        xhr.onload = function() {
+            if (xhr.readyState === 4) {
+                console.log(url);
+                if (xhr.status === 200) {
+                    json = JSON.parse(xhr.responseText);
+                } else {
+                    console.error('readJSON //', xhr.statusText);
+                }
+            }
+        };
+
+        xhr.onerror = function() {
+            console.error('readJSON // ', xhr.statusText);
+        };
+
+        xhr.send(null);
+        return json;
+    };
+})(window, document);
