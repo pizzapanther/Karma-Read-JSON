98d94106a16ad7ef322a220c33074cdc94ca4689
Paul Bailey <paul.m.bailey@gmail.com>
2015-04-23 10:38:14 -0400

    npm

--- karma-read-json.js
+++ karma-read-json.js
@@ -1,5 +1,7 @@
-function readJSON (url) {
-  url = '/base/' + url;
+exports.base = '/base/';
+
+exports.readJSON = function (url, base) {
+  url = exports.base + url;
   
   var xhr = new XMLHttpRequest();
   var json = null;
@@ -7,14 +9,12 @@ function readJSON (url) {
   xhr.open("GET", url, false);
   
   xhr.onload = function (e) {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        json = JSON.parse(xhr.responseText);
-      }
-      
-      else {
-        console.error('readJSON', url, xhr.statusText);
-      }
+    if (xhr.status === 200) {
+      json = JSON.parse(xhr.responseText);
+    }
+    
+    else {
+      console.error('readJSON', url, xhr.statusText);
     }
   };
   
@@ -24,4 +24,4 @@ function readJSON (url) {
   
   xhr.send(null);
   return json;
-}
+};
